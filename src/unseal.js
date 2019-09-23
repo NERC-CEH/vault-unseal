2dba1b8b8562a4fbd9b95f7036662d80bd55abde
Iain <iainw@ceh.ac.uk>
2019-09-23 13:20:35 +0100

    fix: lifting Docker image, switch to use async/await

--- src/unseal.js
+++ src/unseal.js
@@ -1,24 +1,45 @@
 const axios = require('axios');
+const convict = require('convict');
 
-const { VAULT_KEYS, VAULT_ADDR } = process.env;
-const KEYS_ARR = VAULT_KEYS.split(',');
+var config = convict({
+  vaultAddr: {
+    doc: 'Vault Address Location',
+    format: 'String',
+    default: 'http://vault-service',
+    env: 'VAULT_ADDR'
+  },
+  vaultKeys: {
+    doc: 'CSV of Vault Keys',
+    format: 'String',
+    env: 'VAULT_KEYS',
+    default: null,
+  }
+})
+
+const KEYS_ARR = config.get('vaultKeys').split(',');
+const vaultAddress = config.get('vaultAddr');
 
 function sealStatus() {
-  return axios.get(`${VAULT_ADDR}/v1/sys/seal-status`)
-    .then((res) => res.data.sealed);
+  return axios.get(`${vaultAddress}/v1/sys/seal-status`);
 }
 
 function unsealVault(key) {
-  return axios.post(`${VAULT_ADDR}/v1/sys/unseal`, { key });
+  return axios.post(`${vaultAddress}/v1/sys/unseal`, { key });
 }
 
-sealStatus()
-  .then((sealed) => {
+async function unsealChecker() {
+  try {
+    const res = await sealStatus();
+    const { sealed } = res.data;
     if (sealed) {
-      console.log('Vault is sealed, unsealing...');
+      console.log('Vault is currently sealed');
       KEYS_ARR.forEach(unsealVault);
     } else {
       console.log('Vault is currently unsealed');
     }
-  })
-  .catch((err) => console.log(`Error: ${err}. Cannot determine if Vault is sealed.`));
+  } catch(err) {
+    console.log(`Error: ${err}.`);
+  }
+}
+
+unsealChecker();
